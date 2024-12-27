ecf7023a8208282e8a2eb3b37dd72d30b596e9c6
Jared Weiss <jared.weiss@quiq.com>
2024-12-27 10:30:47 -0700

    #NP - move evim -> nvim now that it feels good to me :)

--- nvim-expirimental/init.lua
+++
@@ -1,4 +0,0 @@
-require('modules.options').setup() -- NOTE: options loading should be first here (do not change)
-require('config.lazy')
-require('modules.keybinds').setup {}
-require("modules.utils")
