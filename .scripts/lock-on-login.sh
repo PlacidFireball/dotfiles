a2b6d225010b7626804b62a909ad16e35262a9f6
Jared Weiss <jared.lee.weiss@gmail.com>
2025-01-06 08:12:30 -0700

    remove lock on login since it did jack for me

--- .scripts/lock-on-login.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-/usr/bin/dbus-send --session --type=method_call --dest=org.freedesktop.ScreenSaver /ScreenSaver org.freedesktop.ScreenSaver.Lock &
--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/zsh
 nm-applet & # start network-manager-applet in the background
+/usr/bin/dbus-send --session --type=method_call --dest=org.freedesktop.ScreenSaver /ScreenSaver org.freedesktop.ScreenSaver.Lock &
