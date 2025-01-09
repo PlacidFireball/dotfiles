5f1ced731fb219febb10e2ce9a1facf8c87b6ec9
Jared Weiss <jared.weiss@quiq.com>
2025-01-08 17:11:16 -0700

    #NP - add textobjects because I missed them so bad

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    priority = 2,
     config = function()
       require 'nvim-treesitter.configs'.setup {
         ensure_installed = { "python", "rust", "c", "scala", "java", "lua", "go", "vim", "vimdoc", "markdown", "markdown_inline" },
@@ -19,6 +20,70 @@ return {
               return true
             end
           end
+        },
+      }
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    priority = 1,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.configs").setup {
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["gj"] = "@function.outer",
+              ["]]"] = "@class.outer",
+              ["]b"] = "@block.outer",
+              ["]a"] = "@parameter.inner",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["gJ"] = "@function.outer",
+              ["]["] = "@class.outer",
+              ["]B"] = "@block.outer",
+              ["]A"] = "@parameter.inner",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["gk"] = "@function.outer",
+              ["[["] = "@class.outer",
+              ["[b"] = "@block.outer",
+              ["[a"] = "@parameter.inner",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["gK"] = "@function.outer",
+              ["[]"] = "@class.outer",
+              ["[B"] = "@block.outer",
+              ["[A"] = "@parameter.inner",
+            },
+          },
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ab"] = "@block.outer",
+              ["ib"] = "@block.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+              ["a/"] = "@comment.outer",
+              ["i/"] = "@comment.outer",   -- no inner for comment
+              ["aa"] = "@parameter.outer", -- parameter -> argument
+              ["ia"] = "@parameter.inner",
+            },
+          },
         },
       }
     end
