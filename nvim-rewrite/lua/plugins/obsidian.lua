a8b1a1816d7cf3a7d9c4db84a09274f17b3b6c4d
Jared Weiss <jared.weiss@Jareds-MacBook-Pro-2.local>
2026-04-12 19:06:12 -0700

    keybinds and settings for obsidian.nvim

--- nvim-rewrite/lua/plugins/obsidian.lua
+++ nvim-rewrite/lua/plugins/obsidian.lua
@@ -1,8 +1,6 @@
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*",
-  lazy = true,
-  ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
@@ -14,6 +12,16 @@ return {
           path = "~/Documents/Obsidian/placidfireball/",
         },
       },
+      notes_subdir = 'notes',
+      completion = {
+        blink = true,
+        min_chars = 2,
+        create_new = false,
+      }
     }
+
+    vim.keymap.set('n', '<leader>so', ':ObsidianSearch<CR>', { desc = '[S]earch [O]bsidian'})
+    vim.keymap.set('n', '<leader>nt', ':Obsidian tomorrow<CR>', { desc = 'Make [N]ote for [T]omorrow'})
+    vim.keymap.set('n', '<leader>ny', ':Obsidian yesterday<CR>', { desc = 'Open [N]ote for [Y]esterday'})
   end,
 }
